63bb37deab42e6ed1823d6901000a1edee988e3e
SMART-ROSE Dev <smartrose-dev@local.dev>
2026-07-11 11:21:43 +0700

    chore: kembalikan .env.example sebagai template, tidak ada secret asli

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { ApiError } from '@/utils/apiError';
 import { logger } from '@/utils/logger';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function errorHandler(err: unknown, req: Request, res: Response, _next: NextFunction) {
   if (err instanceof ApiError) {
     return res.status(err.statusCode).json({ message: err.message });
